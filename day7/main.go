52071f71e2415589dea0f40f46ac7a82616cf296
tomzig16 <tzigmantavicius@gmail.com>
2024-12-07 23:50:16 +0200

    Add Day 7 solutions

--- day7/main.go
+++ day7/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"math"
+	"strconv"
 	"strings"
 )
 
@@ -20,11 +21,15 @@ func main() {
 		numbers = append(numbers, n)
 	}
 
-	Part1(numbers)
+	// Part1(numbers)
+	Part1Recursive(numbers)
+	Part2Recursive(numbers)
 	// Part2(lines, startingPositionX, startingPositionY)
 
 }
 
+// Old part 1 solution that broke when part 2 happened due to no good solution for base 3 numbers
+// Leaving for grandchildren to cringe in the future
 func Part1(input [][]int) {
 	finalSum := 0
 	for _, inputNumbers := range input {
@@ -34,6 +39,7 @@ func Part1(input [][]int) {
 
 		counter := 0
 		numberOfIterations := powInt(2, nOfOperators)
+		fmt.Println("Target ", target)
 		for i := 0; i < numberOfIterations; i++ {
 			sum := rightSideNumbers[0]
 			st := "actions: "
@@ -47,6 +53,7 @@ func Part1(input [][]int) {
 					st += "*"
 				}
 			}
+			fmt.Println(strconv.Itoa(i)+": ", st)
 			if sum == target {
 				finalSum += target
 				break
@@ -62,38 +69,79 @@ func powInt(x, y int) int {
 	return int(math.Pow(float64(x), float64(y)))
 }
 
-func Part2(input [][]int) {
+// Recursive method
+// func AddFunc(target int, rightSide []int, nextIndex int) int {
+// 	tempResult := 0
+// 	if nextIndex < len(rightSide) {
+// 		tempResult = AddFunc(target, rightSide, nextIndex+1)
+// 		if tempResult == target {
+// 			return tempResult
+// 		}
+// 		tempResult = MulFunc(target, rightSide, nextIndex+1)
+// 		if tempResult == target {
+// 			return tempResult
+// 		}
+// 	}
+// 	return tempResult
+// }
+
+func GetResult(target int, rightSide []int, tempTotal int, operator string, isPart2 bool) bool {
+	var equationTotal int
+	switch operator {
+	case "+":
+		equationTotal = tempTotal + rightSide[0]
+		break
+	case "*":
+		equationTotal = tempTotal * rightSide[0]
+		break
+	case "||":
+		tempTotalAsStr := strconv.Itoa(tempTotal)
+		rightSideAsStr := strconv.Itoa(rightSide[0])
+		combinedAsStr := tempTotalAsStr + rightSideAsStr
+		equationTotal, _ = strconv.Atoi(combinedAsStr)
+		break
+	}
+
+	if len(rightSide) == 1 {
+		return equationTotal == target
+	}
+
+	sumResult := GetResult(target, rightSide[1:], equationTotal, "+", isPart2)
+	mulResult := GetResult(target, rightSide[1:], equationTotal, "*", isPart2)
+	concatResult := false
+	if isPart2 {
+		concatResult = GetResult(target, rightSide[1:], equationTotal, "||", isPart2)
+	}
+	return sumResult || mulResult || concatResult
+}
+
+func Part1Recursive(input [][]int) {
 	finalSum := 0
 	for _, inputNumbers := range input {
 		target := inputNumbers[0]
 		rightSideNumbers := inputNumbers[1:]
-		operators := make([]rune, len(rightSideNumbers)-1)
-		for i := 0; i < len(operators); i++ {
-			operators[i] = '+'
+
+		if GetResult(target, rightSideNumbers, 0, "+", false) ||
+			GetResult(target, rightSideNumbers, 0, "*", false) {
+			finalSum += target
 		}
 
-		counter := 0
-		numberOfIterations := powInt(3, len(operators))
-		for i := 0; i < numberOfIterations; i++ {
-			sum := rightSideNumbers[0]
-			st := "actions: "
-			for x := 0; x < len(operators) && sum < target; x++ {
-				bitToCheck := 1 << x
-				if (counter & bitToCheck) == 0 {
-					sum += rightSideNumbers[x+1]
-					st += "+"
-				} else {
-					sum *= rightSideNumbers[x+1]
-					st += "*"
-				}
-			}
-			if sum == target {
-				finalSum += target
-				break
-			}
-			counter++
-		}
 	}
-
 	fmt.Println("Result Part 1: ", finalSum)
+}
+
+func Part2Recursive(input [][]int) {
+	finalSum := 0
+	for _, inputNumbers := range input {
+		target := inputNumbers[0]
+		rightSideNumbers := inputNumbers[1:]
+
+		if GetResult(target, rightSideNumbers, 0, "+", true) ||
+			GetResult(target, rightSideNumbers, 0, "*", true) ||
+			GetResult(target, rightSideNumbers, 0, "||", true) {
+			finalSum += target
+		}
+
+	}
+	fmt.Println("Result Part 2: ", finalSum)
 }
