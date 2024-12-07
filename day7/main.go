4ff90e3f3b74b4b60ca967f0aea4c7ae903b642f
tomzig16 <tzigmantavicius@gmail.com>
2024-12-07 22:27:07 +0200

    Solve day 7 part 1

---
+++ day7/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"adventofcode/shared"
+	"fmt"
+	"log"
+	"math"
+	"strings"
+)
+
+func main() {
+	lines, err := shared.ReadLines("input.txt")
+	if err != nil {
+		log.Fatalf("Failed to read input %v", err)
+	}
+	numbers := make([][]int, 0)
+	for _, line := range lines {
+		l := strings.Split(strings.Replace(line, ":", "", 1), " ")
+		n, _ := shared.ConvertStringToInts(l)
+		numbers = append(numbers, n)
+	}
+
+	Part1(numbers)
+	// Part2(lines, startingPositionX, startingPositionY)
+
+}
+
+func Part1(input [][]int) {
+	finalSum := 0
+	for _, inputNumbers := range input {
+		target := inputNumbers[0]
+		rightSideNumbers := inputNumbers[1:]
+		operators := make([]rune, len(rightSideNumbers)-1)
+		for i := 0; i < len(operators); i++ {
+			operators[i] = '+'
+		}
+
+		counter := 0
+		numberOfIterations := powInt(2, len(operators))
+		for i := 0; i < numberOfIterations; i++ {
+			sum := rightSideNumbers[0]
+			st := "actions: "
+			for x := 0; x < len(operators) && sum < target; x++ {
+				bitToCheck := 1 << x
+				if (counter & bitToCheck) == 0 {
+					sum += rightSideNumbers[x+1]
+					st += "+"
+				} else {
+					sum *= rightSideNumbers[x+1]
+					st += "*"
+				}
+			}
+			if sum == target {
+				finalSum += target
+				break
+			}
+			counter++
+		}
+	}
+
+	fmt.Println("Result Part 1: ", finalSum)
+}
+
+func powInt(x, y int) int {
+	return int(math.Pow(float64(x), float64(y)))
+}
+
+// So far seems incorrect
+func Part2(input []string, startX int, startY int) {
+	var visitedCells map[int]map[int][]int = make(map[int]map[int][]int)
+
+	fmt.Println("Result Part 2: ", len(visitedCells))
+}
