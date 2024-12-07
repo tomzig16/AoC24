1fad1d3ea3566584289959799a50993db25a1804
tomzig16 <tzigmantavicius@gmail.com>
2024-12-07 22:32:43 +0200

    Small cleanup and part 2 start

--- day7/main.go
+++ day7/main.go
@@ -30,17 +30,14 @@ func Part1(input [][]int) {
 	for _, inputNumbers := range input {
 		target := inputNumbers[0]
 		rightSideNumbers := inputNumbers[1:]
-		operators := make([]rune, len(rightSideNumbers)-1)
-		for i := 0; i < len(operators); i++ {
-			operators[i] = '+'
-		}
+		nOfOperators := len(rightSideNumbers) - 1
 
 		counter := 0
-		numberOfIterations := powInt(2, len(operators))
+		numberOfIterations := powInt(2, nOfOperators)
 		for i := 0; i < numberOfIterations; i++ {
 			sum := rightSideNumbers[0]
 			st := "actions: "
-			for x := 0; x < len(operators) && sum < target; x++ {
+			for x := 0; x < nOfOperators && sum < target; x++ {
 				bitToCheck := 1 << x
 				if (counter & bitToCheck) == 0 {
 					sum += rightSideNumbers[x+1]
@@ -65,9 +62,38 @@ func powInt(x, y int) int {
 	return int(math.Pow(float64(x), float64(y)))
 }
 
-// So far seems incorrect
-func Part2(input []string, startX int, startY int) {
-	var visitedCells map[int]map[int][]int = make(map[int]map[int][]int)
+func Part2(input [][]int) {
+	finalSum := 0
+	for _, inputNumbers := range input {
+		target := inputNumbers[0]
+		rightSideNumbers := inputNumbers[1:]
+		operators := make([]rune, len(rightSideNumbers)-1)
+		for i := 0; i < len(operators); i++ {
+			operators[i] = '+'
+		}
 
-	fmt.Println("Result Part 2: ", len(visitedCells))
+		counter := 0
+		numberOfIterations := powInt(3, len(operators))
+		for i := 0; i < numberOfIterations; i++ {
+			sum := rightSideNumbers[0]
+			st := "actions: "
+			for x := 0; x < len(operators) && sum < target; x++ {
+				bitToCheck := 1 << x
+				if (counter & bitToCheck) == 0 {
+					sum += rightSideNumbers[x+1]
+					st += "+"
+				} else {
+					sum *= rightSideNumbers[x+1]
+					st += "*"
+				}
+			}
+			if sum == target {
+				finalSum += target
+				break
+			}
+			counter++
+		}
+	}
+
+	fmt.Println("Result Part 1: ", finalSum)
 }
