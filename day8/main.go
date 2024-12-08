c1cb082b1ed6198984fcf60e06e69e178253d4ca
tomzig16 <tzigmantavicius@gmail.com>
2024-12-08 16:07:31 +0200

    Solve Day 8 part 2

--- day8/main.go
+++ day8/main.go
@@ -25,7 +25,7 @@ func main() {
 		log.Fatalf("Failed to read input %v", err)
 	}
 	Part1(lines)
-	// Part2(lines)
+	Part2(lines)
 
 }
 
@@ -76,4 +76,55 @@ func Part1(lines []string) {
 		}
 	}
 	shared.PrintResultsInt(1, len(placedAntinodes))
+}
+
+func Part2(lines []string) {
+	knownAntennas := make(map[rune][]Coordinates)
+	mapWidth := len(lines[0])
+	mapHeight := len(lines)
+
+	for i, line := range lines {
+		for j, slot := range line {
+			if slot == '.' {
+				continue
+			}
+			_, ok := knownAntennas[slot]
+			if !ok {
+				knownAntennas[slot] = make([]Coordinates, 0)
+			}
+			knownAntennas[slot] = append(knownAntennas[slot], Coordinates{row: i, col: j})
+		}
+	}
+
+	placedAntinodes := make([]Coordinates, 0)
+	for _, sameAntennas := range knownAntennas {
+		for i, uniqueAntenna := range sameAntennas {
+			for j := i + 1; j < len(sameAntennas); j++ {
+				vectToNext := uniqueAntenna.GetVectorToNext(sameAntennas[j])
+
+				tempPos := uniqueAntenna
+				for (tempPos.row >= 0 && tempPos.row < mapHeight) &&
+					(tempPos.col >= 0 && tempPos.col < mapWidth) {
+					tempPos.col = tempPos.col - vectToNext.col
+					tempPos.row = tempPos.row - vectToNext.row
+				}
+				tempPos.col += vectToNext.col
+				tempPos.row += vectToNext.row
+
+				for (tempPos.row >= 0 && tempPos.row < mapHeight) &&
+					(tempPos.col >= 0 && tempPos.col < mapWidth) {
+					doesOverlap := slices.ContainsFunc(placedAntinodes, func(coord Coordinates) bool {
+						return (coord.col == tempPos.col) && (coord.row == tempPos.row)
+					})
+					if !doesOverlap {
+						placedAntinodes = append(placedAntinodes, tempPos)
+					}
+					tempPos.col += vectToNext.col
+					tempPos.row += vectToNext.row
+				}
+
+			}
+		}
+	}
+	shared.PrintResultsInt(2, len(placedAntinodes))
 }
